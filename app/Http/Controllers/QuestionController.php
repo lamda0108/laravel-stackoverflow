f4b7a3e18ff15df337f40445f7424c5dc7718f2e
User <user@Users-MBP.hot.rr.com>
2021-02-18 14:58:07 -0600

    Extend the form functionality to be able to update question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -67,7 +67,9 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view("questions.edit")->with([
+            'question'=>$question
+        ]);
     }
 
     /**
@@ -77,9 +79,12 @@ class QuestionController extends Controller
      * @param  \App\Models\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+        return redirect('/question')->with([
+            'success'=>'Your question has been updated'
+        ]);
     }
 
     /**
